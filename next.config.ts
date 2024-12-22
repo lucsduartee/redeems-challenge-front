2ea8ccf47c07ebdc6c12fffccb9705bf9e0b796e
Lucas Duarte <lucs.rkt@gmail.com>
2024-12-22 02:58:01 -0300

    feat: add dependencies to app

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/welcome-step',
+        permanent: true,
+      }
+    ]
+  },
 };
 
 export default nextConfig;
